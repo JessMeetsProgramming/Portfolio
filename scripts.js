06adfe54a7bdf9287a66d62106d7f18727d9d8f1
JessMeetsProgramming <jess16_greeny@hotmail.com>
2022-07-09 11:14:58 +1200

    created hide/show functions

--- scripts.js
+++ scripts.js
@@ -1,38 +1,58 @@
 window.onload = function() {
 
-
+    var imageIndex = 0;
+    var mobIndex = 3;
+    var pcIndex;
+    
     // add event listener to each of the images and call function on click event
     var images = document.getElementsByClassName("images");
     for (i = 0; i < images.length; i++) {
         images[i].addEventListener("click", swapImg);
     }
-
+    
     function swapImg() {
 
-        // get relevant elements
-        var img1 = document.getElementById("img1");
-        var img2 = document.getElementById("img2");
-        var img3 = document.getElementById("img3");
-
-        // get the style values for each image (because they are not inline styles)
-        var styles1 = window.getComputedStyle(img1);
-        var styles2 = window.getComputedStyle(img2);
-        var styles3 = window.getComputedStyle(img3);
-
-        
-        // toggle which image is displayed
-        if (styles1.display == "block") {            
-            img1.style.display = "none";
-            img2.style.display = "block";
-        } else if (styles2.display == "block") {
-            img2.style.display = "none";
-            img3.style.display = "block";
-        } else if (styles3.display == "block") {
-            img3.style.display = "none";
-            img1.style.display = "block";
-        }
+        imageIndex++; 
+        // hide current image
+        images[imageIndex-1].style.display = "none";
+        // on 3rd click reset index 
+        if (imageIndex == 3) {imageIndex = 0} // desktop images
+        if (imageIndex == 6) {imageIndex = 3} // mobile images
+        // show next image
+        images[imageIndex].style.display = "block";
+        // save current index
+        if (imageIndex <= 2) {pcIndex = imageIndex} 
+        if (imageIndex >= 3) {mobIndex = imageIndex} 
     }
 
+
+    //add event listener to icons
+    var pc = document.getElementById("pc");
+    var mob = document.getElementById("mob");
+    pc.addEventListener("click", togglePC);
+    mob.addEventListener("click", toggleMob);
     
+    
+    function toggleMob() {
+               
+        // show mobile images
+        document.getElementById("mobile").style.display = "inline-block";
+        // hide desktop images
+        document.getElementById("desktop").style.display = "none";
+        // set counter to previously saved index
+        imageIndex = mobIndex;
+         
+    }
+
+    function togglePC() {
+
+        //show desktop images
+        document.getElementById("desktop").style.display = "inline-block";
+        // hide mobile images
+        document.getElementById("mobile").style.display = "none";
+        // set counter to previously saved index
+        imageIndex = pcIndex;
+    }
+
 
 }
